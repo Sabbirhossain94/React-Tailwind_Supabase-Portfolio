b2d1df43d5c028f289848a59563ce4ad4efbb101
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2022-10-09 14:47:13 +0600

    route problem fixed

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-
 import './App.css';
-import { useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
 import Projects from './components/pages/Projects';
 
 function App() {
 
-  const [showModal, setShowModal] = useState(false);
-
   return (
 
     <div>
-      <div className=''>
-
-        <main className='p-[50px] bg-white dark:bg-slate-800'>
-
-          <div className=" sm:px-8">
-
-            <div className="mx-auto max-w-7xl lg:px-8">
-
-              <div className="relative px-4 sm:px-8 lg:px-12">
-
-                <div className="mx-auto max-w-2xl lg:max-w-5xl">
-
-                  <Navigation />
-
-                 
-                  <Projects />
-
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
-
-
-
-      </div>
+      
+      <Projects />
     </div>
+
   )
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from '../../App'
 import Projects from "./Projects"
 import Calendar from './Calendar'
+import Navigation from '../Navigation'
 
 export default function Home() {
     return (
         <Router>
-            <Route path="/" element={<App />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/calendar" element={<Calendar />} />
+            <Navigation/>
+            <Routes>          
+                <Route path="/" element={<App />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/calendar" element={<Calendar />} />
+            </Routes>
         </Router>
+
     )
 }
