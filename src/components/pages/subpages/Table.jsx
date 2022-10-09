6de31f31f7fbceaaa18d1ea000b026eca38ab2bf
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2022-10-10 00:52:53 +0600

    some changes in table added

--- src/components/pages/subpages/Table.jsx
+++ src/components/pages/subpages/Table.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 export default function Table() {
 
     const [allprojects, setAllProjects] = useState([])
+
     const getProjects = async (e) => {
         let { data, error } = await supabase
             .from('projects')
@@ -16,9 +17,17 @@ export default function Table() {
             setAllProjects(data)
         }
     }
+    const deleteProject = async (id) => {
+        const { data, error } = await supabase
+            .from('projects')
+            .delete()
+            .match({ id: id })
+    }
     useEffect(() => {
         getProjects()
     }, [])
+    
+
 
     return (
         <div>
@@ -41,7 +50,7 @@ export default function Table() {
                                                     Title
                                                     <span class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
                                                         <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                                            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                                                         </svg>
                                                     </span>
                                                 </a>
@@ -57,7 +66,7 @@ export default function Table() {
                                                     Description
                                                     <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
                                                         <svg className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                                            <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                                                         </svg>
                                                     </span>
                                                 </a>
@@ -67,7 +76,7 @@ export default function Table() {
                                                     Link
                                                     <span className="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
                                                         <svg className="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                                            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                                                         </svg>
                                                     </span>
                                                 </a>
@@ -78,17 +87,18 @@ export default function Table() {
                                         </tr>
                                     </thead>
                                     <tbody className="divide-y divide-gray-200 bg-slate-700">
-                                        {allprojects.map((item) => {
-                                            <tr>
+                                        {allprojects.map((item) => (
+                                            <tr key={item.id}>
                                                 <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-200 sm:pl-6">{item.title}</td>
                                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">{item.image}</td>
                                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">{item.description}</td>
                                                 <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-200">{item.link}</td>
                                                 <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                                    <button  onClick={() => deleteProject(item.id)} class="text-red-600 hover:text-red-900">Delete<span class="sr-only">, Lindsay Walton</span></button>
+                                                    <button  class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></button>
                                                 </td>
                                             </tr>
-                                        })}
+                                        ))}
                                     </tbody>
                                 </table>
                             </div>
