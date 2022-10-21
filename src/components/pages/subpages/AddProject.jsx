7031488fd123f6d9270f7110998e8a6b51aa762b
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2022-10-21 11:39:42 +0600

    some fixes

--- src/components/pages/subpages/AddProject.jsx
+++ src/components/pages/subpages/AddProject.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { portfolioClient } from '../../../portfolioClient';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom'
+import AnimatedPage from '../../AnimatedPages';
 
 export default function AddProject({ session, funcTopNav }) {
 
@@ -70,29 +71,31 @@ export default function AddProject({ session, funcTopNav }) {
 
 
     return (
-        <div>
-            <div className={`mx-auto ring-1 p-6 mt-[100px] rounded-lg w-1/2 md:w-1/2`}>
-                <form onSubmit={(e) => handleSubmit(e)}>
-                    <div className="mb-6">
-                        <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Title</label>
-                        <input type="text" id="text" value={title} onChange={(e) => setTitle(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="imageUrl" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image URL</label>
-                        <input type="text" id="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
-                        <input type="text" id="text" value={description} onChange={(e) => setDescription(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    </div>
-                    <div className="mb-6">
-                        <label htmlFor="link" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Link</label>
-                        <input type="text" id="text" value={link} onChange={(e) => setLink(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                    </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                    <Link to="/dashboard"><button type="button" className="ml-[20px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button></Link>
-                </form>
+        <AnimatedPage>
+            <div>
+                <div className={`mx-auto ring-1 p-6 mt-[100px] rounded-lg w-1/2 md:w-1/2`}>
+                    <form onSubmit={(e) => handleSubmit(e)}>
+                        <div className="mb-6">
+                            <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Title</label>
+                            <input type="text" id="text" value={title} onChange={(e) => setTitle(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="imageUrl" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Image URL</label>
+                            <input type="text" id="text" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
+                            <input type="text" id="text" value={description} onChange={(e) => setDescription(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="link" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Link</label>
+                            <input type="text" id="text" value={link} onChange={(e) => setLink(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                        </div>
+                        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <Link to="/dashboard"><button type="button" className="ml-[20px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Close</button></Link>
+                    </form>
+                </div>
             </div>
-        </div>
+        </AnimatedPage>
     )
 }
--- src/components/pages/subpages/Gallery.jsx
+++ src/components/pages/subpages/Gallery.jsx
@@ -56,31 +56,32 @@ export default function Gallery() {
 
     return (
         <div>
-            <section className="overflow-hidden text-gray-700 ">
-                <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
-                    <div className="flex flex-wrap -m-1 md:-m-2">
-                        <div className="flex flex-wrap w-1/3">
-                            <div className="w-full p-1 md:p-2">
-                                <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+            <div className=' w-full mx-auto flex justify-center'>
+                {/* <section className="overflow-hidden text-gray-700 ">
+                    <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+                        <div className="flex flex-wrap -m-1 md:-m-2">
+                            <div className="flex flex-wrap w-1/3">
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/3">
-                            <div className="w-full p-1 md:p-2">
-                                <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+                            <div className="flex flex-wrap w-1/3">
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex flex-wrap w-1/3">
-                            <div className="w-full p-1 md:p-2">
-                                <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                    src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+                            <div className="flex flex-wrap w-1/3">
+                                <div className="w-full p-1 md:p-2">
+                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
+                                        src="https://aliltdblkhwtxvwqhipo.supabase.co/storage/v1/object/projects/public/tuku.png" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-            {/* <div className='w-1/5 flex flex-col mx-auto mt-[100px]'>
+                </section> */}
+                {/* <div className='w-1/5 flex flex-col mx-auto mt-[100px]'>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 bg-blu" htmlFor="file_input">Upload file</label>
                 <input className="block w-full text-sm text-gray-900 bg-blue-500 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                     id="file_input"
@@ -89,33 +90,29 @@ export default function Gallery() {
                 />
                 <button>Upload</button>
             </div> */}
-            <div className='w-1/5 flex flex-col mx-auto' style={{}} aria-live="polite">
-                <img
-                    src="https://i.imgur.com/W2AT377.jpg"
-                    alt=""
-                    className="avatar image ring-1"
-                    style={{}}
-                />
-                {uploading ? (
-                    'Uploading...'
-                ) : (
-                    <>
+                <div className='w-full ' style={{}} aria-live="polite">
+                    <img
+                        src="https://i.imgur.com/W2AT377.jpg"
+                        alt=""
+                        className="avatar image ring-1"
+                        style={{width: "200px",height: "200px"}}
+                    />
 
-                        <div className="mt-[15px]">
-                            <input
-                                type="file"
-                                id="single"
-                                accept="image/*"
-                                disabled={uploading}
-                                className="block w-full text-sm text-gray-900 bg-blue-500 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            />
-                        </div>
-                        <button onClick={uploadImage} className='mt-[15px] flex justify-center rounded-md border border-transparent bg-sky-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>Upload</button>
+                    <div className="mt-[15px]">
+                        <input
+                            type="file"
+                            id="single"
+                            accept="image/*"
+                            disabled={uploading}
+                            className="block w-full text-sm text-gray-900 bg-blue-500 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        />
+                    </div>
+                    <button onClick={uploadImage} className='w-full mt-[15px] flex justify-center rounded-md border border-transparent bg-sky-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>Upload</button>
 
-                    </>
-                )}
+
+                </div>
+
             </div>
-
         </div>
     )
 }
