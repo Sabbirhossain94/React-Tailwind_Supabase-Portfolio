5b4c905b1d68b931ee752ff694949d3e02a32698
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2025-01-20 16:44:55 +0600

    contact page button width fixed for small screen

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -171,7 +171,7 @@ export default function Contact() {
                   <div className="sm:col-span-2 sm:flex sm:justify-end">
                     <button
                       type="submit"
-                      className="inline-flex items-center gap-2 justify-center rounded-md py-3 px-8 text-sm outline-offset-2 transition active:transition-none bg-zinc-200 font-medium text-zinc-900 hover:text-sky-400 hover:bg-zinc-200/50 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800 dark:text-zinc-300 dark:hover:bg-zinc-900/50 dark:hover:text-teal-500 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-2 w-1/4"
+                      className="inline-flex items-center gap-2 justify-center rounded-md py-3 px-8 text-sm outline-offset-2 transition active:transition-none bg-zinc-200 font-medium text-zinc-900 hover:text-sky-400 hover:bg-zinc-200/50 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800 dark:text-zinc-300 dark:hover:bg-zinc-900/50 dark:hover:text-teal-500 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-2 w-full sm:w-1/4"
                     >
                       {loading ? <><Spinner /> Processing...</> : "Submit"}
                     </button>
