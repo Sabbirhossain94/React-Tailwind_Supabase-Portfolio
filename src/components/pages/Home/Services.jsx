b85dabccbf523fbe5e982d2c7dae0d644dc64e33
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2025-01-27 12:49:02 +0600

    animation issue fix service section

--- src/components/pages/Home/Services.jsx
+++ src/components/pages/Home/Services.jsx
@@ -15,19 +15,19 @@ export default function Services() {
           </h2>
           <div className="mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-10 ">
             <div
-              // data-aos="fade-right"
+              data-aos={window.innerWidth < 640 ? "fade-up" : "fade-right"}
               className="relative border dark:border-zinc-700/90 rounded-xl col-span-1 w-80 sm:w-96 md:w-80 lg:w-72 xl:w-80 h-96 flex flex-col justify-center items-center">
               <WebDesign className="w-20 h-20 lg:w-16 xl:w-20 lg:h-16 xl:h-20 text-sky-400 dark:text-teal-500" />
-              <span className="dark:text-white mt-4">Web Design</span>
+              <span className="dark:text-white mt-4">Figma To Website</span>
             </div>
             <div
-              // data-aos="fade-up"
+              data-aos="fade-up"
               className="relative border dark:border-zinc-700/90 rounded-xl col-span-1 w-80 sm:w-96 md:w-80 lg:w-72 xl:w-80 h-96 flex flex-col justify-center items-center">
               <Frontend className="w-20 h-20 lg:w-16 xl:w-20 lg:h-16 xl:h-20 text-sky-400 dark:text-teal-500" />
               <span className="dark:text-white mt-4">Frontend Development</span>
             </div>
             <div
-              // data-aos="fade-left"
+              data-aos={window.innerWidth < 640 ? "fade-up" : "fade-left"}
               className="relative border dark:border-zinc-700/90 rounded-xl col-span-1 w-80 sm:w-96 md:w-80 lg:w-72 xl:w-80 h-96 flex flex-col justify-center items-center">
               <FullStack className="w-20 h-20 lg:w-16 xl:w-20 lg:h-16 xl:h-20 text-sky-400 dark:text-teal-500" />
               <span className="dark:text-white mt-4">Full Stack Development</span>
